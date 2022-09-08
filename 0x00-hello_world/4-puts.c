331fa42c28dc039593cdb02c85aa0ab486fc4cc0
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-08 11:43:10 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
+ * main - print to the screen using puts
+ * Return: it return value zero
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
