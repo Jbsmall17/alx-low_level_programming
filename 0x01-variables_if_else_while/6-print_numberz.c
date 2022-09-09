91403f69c2440af29f94b315e98463cc5f4039a0
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-09 10:12:03 -0700

    commit 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the screen
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
