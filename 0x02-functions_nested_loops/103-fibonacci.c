30b8cc595225bf6012323b8c2cbf02d757365dcb
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-14 14:40:29 -0700

    advance

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print to the screen
+ * Return: returns a number
+ */
+
+int main(void)
+{
+	long int a = 1;
+	long int b = 1;
+	long int c = a + b;
+	long int d = 0;
+
+	while (c < 4000000)
+	{
+		if (c % 2 == 0)
+			d += c;
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	printf("%ld\n", d);
+	return (0);	
+}
