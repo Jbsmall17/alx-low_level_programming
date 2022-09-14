f597d9ccacdb39cd1460f879d0b97bdeeb866cc6
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-14 11:51:58 -0700

    advance

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print to the screen
+ * Return: returns void
+ */
+
+void main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c, d, e;
+
+	printf("%d, %d, ", a, b);
+	for (c = 2; c < 50; c++)
+	{
+		d = a + b;
+		if (c == 49)
+			printf("%d", d);
+		else
+		{
+			printf("%d, ", d);
+			a = b;
+
+			b = d;
+
+		}
+	}
+	printf("\n");
+}
