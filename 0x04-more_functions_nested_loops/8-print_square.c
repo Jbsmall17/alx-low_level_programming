6965bea609006d39398443fed686329980a500be
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-15 09:35:39 -0700

    print n area square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,11 +8,15 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
+		}
+		if (i == size)
+		{
+			break;
 		}
 		_putchar('\n');
 	}
