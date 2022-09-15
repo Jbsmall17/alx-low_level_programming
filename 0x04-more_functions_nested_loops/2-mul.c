52c30d65cab71d52ad9f2cd5087079dcddfad489
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-15 07:09:31 -0700

    multipies two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,12 @@
  * mul - multiplies two digit
  * @a: first operand
  * @b: second operand
+ * Return: a number
  */
 int mul(int a, int b)
 {
-	int c = a * b;
+	int c;
 
+	c = a * b;
 	return (c);
 }
