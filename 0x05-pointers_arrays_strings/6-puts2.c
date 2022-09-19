8cab70b38a9a6760d8c7599a2c125a24778bddaf
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-19 10:00:05 -0700

    print every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,32 @@
 #include "main.h"
 
 /**
- * puts2 - print every other character of a string 
+ * puts2 - print every other character of a string
  * @str: store a string
  */
 void puts2(char *str)
 {
-	int i;
+	int i = string_length(str)
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (j = 0; j < i; j += 2)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
+}
+
+/**
+ * string_length - print length of a string
+ * @word: stores a string
+ * Return: return a number
+ */
+
+int string_length(char *word)
+{
+	int length;
+
+	for (length = 0; word[length] != '\0'; length++)
+	{
+	}
+	return (length);
 }
