f2b893c36dab5488ab71ff0a1c412713710cd318
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-19 08:02:40 -0700

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - print a reverse string
+ * @s: stores a string
+ */
+void rev_string(char *s)
+{
+	int j = string_length(s);
+	int k;
+
+	for (k = j - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+}
+
+/**
+ * string_length - print the length of a string
+ * @l: store a string
+ * Return: returns a number
+ */
+int string_length(char *l)
+{
+	int i;
+
+	for (i = 0; l[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
