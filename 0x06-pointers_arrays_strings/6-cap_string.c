92234c77fb60c23592ed697792c32f85a018b847
Alao Abdulmusawwir Ajibola <alaomiyaki17@gmail.com>
2022-09-22 08:06:30 -0700

    Capitalize A string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,56 +2,33 @@
 
 /**
  * cap_string - capitalize words in a string
- * @str: string
+ * @s: string
  * Return: returns a string
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
+	int i, j, separatorFound;
 
-	for (i = 0; str[i] != '\0'; i++)
+	separatorFound = 1;
+
+	for (i = 0; s[i]; ++i)
 	{
-		if (i == 0)
+		if (separatorFound && s[i] >= 'a' && s[i] <= 'z')
 		{
-			if (str[i] >= 97 && str[i] <= 122)
+			s[i] = s[i] -  32;
+		}
+		separatorFound = 0;
+		for (j = 0; j < 12; j++)
+		{
+			if (s[i] == '\t' || s[i] == '\n' || s[i] == ','
+			     || s[i] == '\"' || s[i] == '.' || s[i] == '!' ||
+			     s[i] == '{' || s[i] == '}' || s[i] == '(' ||
+			     s[i] == ')' || s[i] == ' ' || s[i] == '?')
 			{
-				str[i] = str[i] - 32;
-				continue;
+				separatorFound = 1;
 			}
 		}
-		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
-		switch (str[i])
-		{
-			case '.':
-			case '\n':
-			case '\t':
-			case ' ':
-			case ',':
-			case ';':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-				++i;
-				if (str[i] >= 97 && str[i] <= 122)
-				{
-					str[i] = str[i] - 32;
-					continue;
-				}
-				break;
-		}
-
 	}
-	return (str);
+	return (s);
 }
-
